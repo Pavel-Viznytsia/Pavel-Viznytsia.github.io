54f851e4ad092e3364795d5304eb390cb8b37085
Pavel_Viznytsia <viznitsap@gmail.com>
2018-04-20 17:03:36 +0300

    added exit point if tickets are not enought

--- js-group-3-4/module-1/js/index.js
+++ js-group-3-4/module-1/js/index.js
@@ -11,26 +11,30 @@ let userPick = Number(msg);
 isPickValid = userPick > 0 && !Number.isNaN(userPick);
 
 if (isPickValid) {
-  if (userPick <= toTaba && window.confirm(`Ты готов поехать на ${TABA}`)) {
-    toTaba = toTaba - userPick;
-    console.log('toTaba ', toTaba);
-    alert(`Приятного путешествия в группе на ${TABA}`);
-  } else if (
-    userPick <= toSharm &&
-    window.confirm(`Ты готов поехать на ${SHARM}`)
-  ) {
-    toSharm = toSharm - userPick;
-    console.log('toSharm ', toSharm);
-    alert(`Приятного путешествия в группе на ${SHARM}`);
-  } else if (
-    userPick <= toHurgada &&
-    window.confirm(`Ты готов поехать на ${HURGADA}`)
-  ) {
-    toHurgada = toHurgada - userPick;
-    console.log('toHurgada ', toHurgada);
-    alert(`Приятного путешествия в группе на ${HURGADA}`);
+  if (userPick > toHurgada) {
+    alert(`Извините, мест нет.`);
   } else {
-    alert(`Нам очень жаль, приходите еще!`);
+    if (userPick <= toTaba && window.confirm(`Ты готов поехать на ${TABA}`)) {
+      toTaba = toTaba - userPick;
+      console.log('toTaba ', toTaba);
+      alert(`Приятного путешествия в группе на ${TABA}`);
+    } else if (
+      userPick <= toSharm &&
+      window.confirm(`Ты готов поехать на ${SHARM}`)
+    ) {
+      toSharm = toSharm - userPick;
+      console.log('toSharm ', toSharm);
+      alert(`Приятного путешествия в группе на ${SHARM}`);
+    } else if (
+      userPick <= toHurgada &&
+      window.confirm(`Ты готов поехать на ${HURGADA}`)
+    ) {
+      toHurgada = toHurgada - userPick;
+      console.log('toHurgada ', toHurgada);
+      alert(`Приятного путешествия в группе на ${HURGADA}`);
+    } else {
+      alert(`Нам очень жаль, приходите еще!`);
+    }
   }
 } else {
   alert(`Ошибка ввода`);
