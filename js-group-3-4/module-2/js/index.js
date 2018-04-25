595d76c0ce0cb38fba5e06acd2ad5047443fb782
Pavel_Viznytsia <viznitsap@gmail.com>
2018-04-25 10:43:52 +0300

    mend

--- js-group-3-4/module-2/js/index.js
+++ js-group-3-4/module-2/js/index.js
@@ -3,10 +3,7 @@ const passwords = ['qweqwe', 'qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let passTryCount = 3;
 
 do {
-  let pass = prompt(
-    `Неверный пароль, у вас осталось [${passTryCount}] попыток`,
-    '',
-  );
+  let pass = prompt(`Введите пароль, у вас есть [${passTryCount}] попытки`, '');
   if (passwords.includes(pass)) {
     alert('Добро пожаловать!');
   } else {
