98a1941c10409df044741e53af592034de020643
Pavel_Viznytsia <viznitsap@gmail.com>
2018-04-25 10:09:24 +0300

    module-2 completed

---
+++ js-group-3-4/module-2/js/index.js
@@ -0,0 +1,18 @@
+const passwords = ['qweqwe', 'qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+
+let passTryCount = 3;
+
+do {
+  let pass = prompt(
+    `Неверный пароль, у вас осталось [${passTryCount}] попыток`,
+    '',
+  );
+  if (passwords.includes(pass)) {
+    alert('Добро пожаловать!');
+  } else {
+    console.log('kO');
+    passTryCount--;
+  }
+} while (passTryCount > 0);
+
+alert('You have NO attempts');
