426589289b917f12a19cefc1f3804edfd638a53b
Pavel_Viznytsia <pavel.viznytsia@progforce.com>
2018-04-26 16:54:25 +0300

    Module-2 added if(state) fixed passTryCount--

--- js-group-3-4/module-2/js/index2.js
+++ js-group-3-4/module-2/js/index2.js
@@ -5,14 +5,15 @@ let passTryCount = 3;
 do {
   let pass = prompt(`Введите пароль, у вас есть [${passTryCount}] попытки`, '');
   passTryCount--;
-  if (pass === null) {
-    break;
-  } else {
+  if (pass !== null) {
     if (passwords.includes(pass)) {
       alert('Добро пожаловать!');
       break;
-    } else if (passTryCount <= 0) {
+    }
+    if (passTryCount < 1) {
       alert('У вас закончились попытки, аккаунт заблокирован!');
     }
+  } else {
+    break;
   }
 } while (passTryCount > 0);
