a25bfaf0fc8d1756eee3e1a6e6cdfc838d26b05c
Pavel_Viznytsia <pavel.viznytsia@progforce.com>
2018-07-31 17:55:01 +0300

    Added some variables

---
+++ js-group-3-4/module-10-JSON. AJAX. Fetch API/js/index.js
@@ -0,0 +1,8 @@
+const getAllBtn = document.querySelector('.getAllBtn');
+const inputGetByName = document.querySelector('.getByName');
+const inputGetByAge = document.querySelector('.getByAge');
+const addBtn = document.querySelector('.addBtn');
+const delBtn = document.querySelector('.delBtn');
+const updateBtn = document.querySelector('.updateBtn');
+
+
