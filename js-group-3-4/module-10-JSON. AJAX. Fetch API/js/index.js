999647dff683ea868f38e838ed163d56c6fa39be
Pavel_Viznytsia <pavel.viznytsia@progforce.com>
2018-08-06 10:51:15 +0300

    Completed Module-10

--- js-group-3-4/module-10-JSON. AJAX. Fetch API/js/index.js
+++ js-group-3-4/module-10-JSON. AJAX. Fetch API/js/index.js
@@ -1,94 +1,155 @@
-const getAllBtn = document.querySelector('.getAllBtn');
-const inputId = document.querySelector('.inputId');
-const getByIdBtn = document.querySelector('.getByIdBtn');
-const inputGetByName = document.querySelector('.getByName');
-const inputGetByAge = document.querySelector('.getByAge');
-const addBtn = document.querySelector('.addBtn');
-const delBtn = document.querySelector('.delBtn');
-const updateBtn = document.querySelector('.updateBtn');
-const outData = document.querySelector('.outData');
+const getAllBtn = document.querySelector( '.getAllBtn' );
+const inputId = document.querySelector( '.inputId' );
+const getByIdBtn = document.querySelector( '.getByIdBtn' );
+const inputGetByName = document.querySelector( '.getByName' );
+const inputGetByAge = document.querySelector( '.getByAge' );
+const addBtn = document.querySelector( '.addBtn' );
+const delBtn = document.querySelector( '.delBtn' );
+const updateBtn = document.querySelector( '.updateBtn' );
+const result = document.querySelector( '.result' );
 const url = 'https://test-users-api.herokuapp.com/users/';
 
-getAllBtn.addEventListener('click', getAllUsers);
-getByIdBtn.addEventListener('click', getUserById);
-addBtn.addEventListener('click', addUser);
-delBtn.addEventListener('click', deleteUser);
-updateBtn.addEventListener('click', updateUser);
+getAllBtn.addEventListener( 'click', getAllUsers );
+getByIdBtn.addEventListener( 'click', getUserById );
+addBtn.addEventListener( 'click', addUser );
+delBtn.addEventListener( 'click', deleteUser );
+updateBtn.addEventListener( 'click', updateUser );
 
-function getAllUsers(evt) {
+function getAllUsers ( evt ) {
   evt.preventDefault();
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      if (data.errors.length === 0) return data;
-      throw new Error(`Error: ${data.errors}. Status: ${data.status}`);
-    })
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+  fetch( url )
+    .then( response => response.json() )
+    .then( data => {
+      if ( data.errors.length === 0 ) return data;
+      throw new Error( `Error: ${ data.errors }. Status: ${ data.status }` );
+    } )
+    .then( data => {
+      result.innerHTML = null;
+      let rowData = '';
+      data.data.forEach( item => {
+        const outData = `
+        <tr>
+          <td>${ item.id }</td>
+          <td>${ item.name }</td>
+          <td>${ item.age }</td>
+        </tr>`;
+        rowData += outData;
+      } );
+      result.innerHTML = rowData;
+    } )
+    .catch( err => console.error( err ) );
 }
 
-function getUserById(evt) {
+function getUserById ( evt ) {
   evt.preventDefault();
-  fetch(`${url}${inputId.value}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data.errors.length === 0) return data;
-      throw new Error(`Error: ${data.errors}. Status: ${data.status}`);
-    })
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+  fetch( `${ url }${ inputId.value }` )
+    .then( response => response.json() )
+    .then( data => {
+      if ( data.errors.length === 0 ) return data;
+      throw new Error( `Error: ${ data.errors }. Status: ${ data.status }` );
+    } )
+    .then( data => {
+      result.innerHTML = null;
+      const item = data.data;
+      const outData = `
+        <tr>
+          <td>${ item.id }</td>
+          <td>${ item.name }</td>
+          <td>${ item.age}</td>
+        </tr>`;
+      rowData = outData;
+      result.innerHTML = rowData;
+    } )
+    .catch( err => console.error( err ) );
 }
 
-function addUser(evt) {
+function addUser ( evt ) {
   evt.preventDefault();
-  fetch(url, {
+  fetch( url, {
     method: 'POST',
-    body: JSON.stringify({
-      name: `${inputGetByName.value}`,
-      age: `${inputGetByAge.value}`,
-    }),
+    body: JSON.stringify( {
+      name: `${ inputGetByName.value }`,
+      age: `${ inputGetByAge.value }`,
+    } ),
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.errors.length === 0) return data;
-      throw new Error(`Error: ${data.errors}. Status: ${data.status}`);
-    })
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+  } )
+    .then( response => response.json() )
+    .then( data => {
+      if ( data.errors.length === 0 ) return data;
+      throw new Error( `Error: ${ data.errors }. Status: ${ data.status }` );
+    } )
+    .then( data => {
+      result.innerHTML = null;
+      const item = data.data;
+      const outData = `
+        <tr>
+          <td>${ item._id }</td>
+          <td>${ item.name }</td>
+          <td>${ item.age }</td>
+        </tr>`;
+      rowData = outData;
+      result.innerHTML = rowData;
+    } )
+    .catch( err => console.error( err ) );
 }
 
-function deleteUser(evt) {
+function deleteUser ( evt ) {
   evt.preventDefault();
-  fetch(`${url}${inputId.value}`, {
+  fetch( `${ url }${ inputId.value }`, {
     method: 'DELETE',
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.errors.length === 0) return data;
-      throw new Error(`Error: ${data.errors}. Status: ${data.status}`);
-    })
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+  } )
+    .then( response => response.json() )
+    .then( data => {
+      if ( data.errors.length === 0 ) return data;
+      throw new Error( `Error: ${ data.errors }. Status: ${ data.status }` );
+    } )
+    .then( data => {
+      result.innerHTML = null;
+      const item = data.data;
+      const outData = `
+        <tr>
+          <td>${ item.id }</td>
+          <td>${ item.name }</td>
+          <td>${ item.age }</td>
+        </tr>`;
+      rowData = outData;
+      result.innerHTML = rowData;
+    } )
+    .catch( err => console.error( err ) );
 }
 
-function updateUser(evt) {
+function updateUser ( evt ) {
   evt.preventDefault();
-  fetch(`${url}${inputId.value}`, {
+  fetch( `${ url }${ inputId.value }`, {
     method: 'PUT',
-    body: JSON.stringify({
-      name: `${inputGetByName.value}`,
-      age: `${inputGetByAge.value}`,
-    }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.errors.length === 0) return data;
-      throw new Error(`Error: ${data.errors}. Status: ${data.status}`);
-    })
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+    body: JSON.stringify( {
+      name: `${ inputGetByName.value }`,
+      age: `${ inputGetByAge.value }`,
+    } ),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  } )
+    .then( response => response.json() )
+    .then( data => {
+      if ( data.errors.length === 0 ) return data;
+      throw new Error( `Error: ${ data.errors }. Status: ${ data.status }` );
+    } )
+    .then( data => {
+      result.innerHTML = null;
+      const item = data.data;
+      const outData = `
+        <tr>
+          <td>${ item.id }</td>
+          <td>${ item.name }</td>
+          <td>${ item.age }</td>
+        </tr>`;
+      rowData = outData;
+      result.innerHTML = rowData;
+    } )
+    .catch( err => console.error( err ) );
 }
